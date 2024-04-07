b23797396876a60de3d00bb872f83216ec878f76
Jiahao Xiang <934622645@qq.com>
2024-04-07 14:22:02 +0800

    easy

---
+++ 453.最小操作次数使数组元素相等.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode.cn id=453 lang=python3
+#
+# [453] 最小操作次数使数组元素相等
+#
+
+# @lc code=start
+class Solution:
+    def minMoves(self, nums: List[int]) -> int:
+        min_num = min(nums)
+        return sum([num - min_num for num in nums])
+# @lc code=end
+
